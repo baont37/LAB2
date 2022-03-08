16697626fc61acfccaa705cc04acc275df339e90
baont37 <baontde150158@fpt.edu.vn>
2022-03-08 22:22:41 +0700

    add project

---
+++ LAB2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LAB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dtgStudent = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtClassID = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(138, 25);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(203, 75);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load table Student on DataGridView";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dtgStudent
+            // 
+            this.dtgStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgStudent.Location = new System.Drawing.Point(56, 147);
+            this.dtgStudent.Name = "dtgStudent";
+            this.dtgStudent.RowTemplate.Height = 25;
+            this.dtgStudent.Size = new System.Drawing.Size(379, 242);
+            this.dtgStudent.TabIndex = 1;
+            this.dtgStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgStudent_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(499, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Student ID";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(589, 181);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(184, 23);
+            this.txtStudentID.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(499, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Name";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(589, 226);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(184, 23);
+            this.txtStudentName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(499, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Class ID";
+            // 
+            // txtClassID
+            // 
+            this.txtClassID.Location = new System.Drawing.Point(589, 271);
+            this.txtClassID.Name = "txtClassID";
+            this.txtClassID.Size = new System.Drawing.Size(184, 23);
+            this.txtClassID.TabIndex = 3;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(499, 317);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(78, 29);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(604, 317);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(78, 29);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(709, 317);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(78, 29);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtClassID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgStudent);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dtgStudent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClassID;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ LAB2/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace LAB2
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionStrings = "Server=localhost;Database=STUDENT_MANAGEMENT;User Id = sa; password=123456";
+        SqlDataAdapter adapter = null;
+        DataSet ds = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            adapter = new SqlDataAdapter("Select * from Student", connectionStrings);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "Student");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            dtgStudent.DataSource = ds.Tables[0];
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            DataRow row = ds.Tables[0].NewRow();
+            row[0] = txtStudentID.Text;
+            row[1] = txtStudentName.Text;
+            row[2] = txtClassID.Text;
+            
+            ds.Tables[0].Rows.Add(row);
+            try
+            {
+                result = adapter.Update(ds.Tables[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Insert failed \n" + ex.Message);
+            }
+            if (result > 0) MessageBox.Show("Insert Successfully");
+        }
+
+        int position = -1;
+        private void dtgStudent_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            position = e.RowIndex;
+            if (position == -1)
+            {
+                MessageBox.Show("No row is selected");
+                return;
+            }
+
+            DataRow row = ds.Tables[0].Rows[position];
+            txtStudentID.Text = row[0].ToString();
+            txtStudentName.Text = row[1].ToString();
+            txtClassID.Text = row[2].ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (position == -1)
+            {
+                MessageBox.Show("No row is selected");
+                return;
+            }
+
+            DataRow row = ds.Tables[0].Rows[position];
+            row.BeginEdit();
+            row[0] = txtStudentID.Text;
+            row[1] = txtStudentName.Text;
+            row[2] = txtClassID.Text;
+            row.EndEdit();
+            int result = adapter.Update(ds.Tables[0]);
+            if (result > 0)
+            {
+                MessageBox.Show("Update successfully!!!");
+            }
+            else
+            {
+                MessageBox.Show("Update failed!!!");
+            }
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (position == -1) return;
+
+            DataRow row = ds.Tables[0].Rows[position];
+            row.Delete();
+            int result = adapter.Update(ds.Tables[0]);
+            if (result > 0)
+            {
+                MessageBox.Show("Delete successfully!!!");
+            }
+            else
+            {
+                MessageBox.Show("Delete failed!!!");
+            }
+        }
+    }
+}
